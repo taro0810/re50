b44cb27b2d08e80cbd03c080451cc0adee47baf0
“taro0810” <taro0810@gmail.com>
2025-12-13 22:32:00 +0900

    astro.config.mjsのURLを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://re50.pages.dev/",
   integrations: [preact()]
 });
